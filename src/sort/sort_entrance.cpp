0fe3b4006a29824c8c787fae6b57f56910e18fee
Phil <zhangxiaofei0829@gmail.com>
2022-03-30 20:27:04 +0800

    bubble

--- src/sort/sort_entrance.cpp
+++ src/sort/sort_entrance.cpp
@@ -3,9 +3,10 @@
 
 void sort()
 {
-	int arr[] = { 9, 13, 6, 5, 2, 3, 8, 4, 7 };
+	int arr[] = { 9, 13, 6, 5, 2, 3, 8, 4 };
 	int len = sizeof(arr) / sizeof(int);
 	std::cout << "arr size: " << len << std::endl;
+
 	print_arr(arr, len);
 	process_time pt(true);
 
