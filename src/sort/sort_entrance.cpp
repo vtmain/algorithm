7cb828b7a0a212057589b19f9332082e9216420e
Phil <zhangxiaofei0829@gmail.com>
2022-04-01 17:01:52 +0800

    init rand array

--- src/sort/sort_entrance.cpp
+++ src/sort/sort_entrance.cpp
@@ -1,17 +1,45 @@
 #include "sort_entrance.h"
 #include "../algorithm.h"
+#include <algorithm>
 
 void sort()
 {
-	int arr[] = { 9, 13, 6, 5, 2, 3, 8, 4, 7 };
+	int arr[20] = {};
 	int len = sizeof(arr) / sizeof(int);
-	std::cout << "arr size: " << len << std::endl;
 
+	for (int i = 0; i < len; i++)
+		arr[i] = i + 1;
+
+#ifdef _WIN32
+	srand(GetTickCount64());
+#else
+	srand(time(0));
+#endif
+
+	for (int k = len - 1; k >= 0; k--)
+	{
+		int n = rand() % (k + 1);
+//		if (k == len - 1)
+//			std::cout << "random num: <" << n;
+//		else
+//			std::cout << ", " << n;
+
+		int tmp = arr[k];
+		arr[k] = arr[n];
+		arr[n] = tmp;
+	}
+//	std::cout << ">" << std::endl;
+
+	std::cout << "arr size: " << len << std::endl;
 	print_arr(arr, len);
 	process_time pt(true);
 
+	////////////////////////////////////////////////
+
 	bubble(arr, len);
 	//quick(arr, len);
+
+	////////////////////////////////////////////////
 
 	print_arr(arr, len);
 }
