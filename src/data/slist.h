2c184932de526fe723e9de3ce826885126aa84f2
Phil <zhangxiaofei0829@gmail.com>
2022-04-02 19:54:03 +0800

    list

---
+++ src/data/slist.h
@@ -0,0 +1,27 @@
+﻿/************************************************************************
+*  FileName:	slist
+*  Description:	单链表
+*  Author:		phil
+*  Date:		2022-04-02
+*  Version:		
+************************************************************************/
+#pragma once
+#include <string>
+#include <vector>
+#include <map>
+
+class ListNode
+{
+public:
+	ListNode()
+		: data(""), next(nullptr)
+	{}
+
+	~ListNode() {}
+
+public:
+	std::string data;
+	ListNode* next;
+};
+
+typedef ListNode* LinkList;
