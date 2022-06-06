50ebc1090a873e194cc89551398a163824e3ccb0
Phil <zhangxiaofei0829@gmail.com>
2022-06-06 21:50:04 +0800

    rb tree

---
+++ src/data/rb_tree.h
@@ -0,0 +1,28 @@
+﻿/************************************************************************
+*  FileName:	rb_tree
+*  Description:	红黑树
+*  Author:		phil
+*  Date:		2022-06-06
+*  Version:		
+************************************************************************/
+#pragma once
+#include <string>
+#include <vector>
+#include <map>
+#include <queue>
+#include "binary_tree.h"
+
+class RBTree : BinaryTree
+{
+public:
+	RBTree()
+		: BinaryTree()
+	{}
+
+	RBTree(int val)
+		: BinaryTree(val)
+	{}
+
+	virtual ~RBTree() {}
+
+};
